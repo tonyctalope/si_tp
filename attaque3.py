8e5c04f3f177bfab78d9728dd920baab8bcd145b
Prime Tony <tony.prime@ecole2600.com>
2023-12-20 15:05:30 +0100

    attaque3+4

--- attaque3.py
+++ attaque3.py
@@ -2,25 +2,13 @@ from pymodbus.client import ModbusTcpClient
 from constant import Register
 import time
 
+register = Register()
+
 client = ModbusTcpClient("192.168.15.1")
 client.connect()
 
-register = Register()
-
-pathR = [50, 27, 30, 33, 60, 77]
-pathL = [12, 17, 0, 47, 55, 63]
-
-frame_time = 3
-force_value_delta = 1 / 20
-
-for frame in range(frame_time * len(pathR)):
-    idx = frame // frame_time
-    for _ in range(int(1 / force_value_delta)):
-        r_value = client.read_holding_registers(register.r_rub).registers[0]
-        l_value = client.read_holding_registers(register.l_rub).registers[0]
-        idx = frame // frame_time
-        if l_value != pathL[idx]:
-            client.write_registers(register.l_rub, pathL[idx])
-        if r_value != pathR[idx]:
-            client.write_registers(register.r_rub, pathR[idx])
-        time.sleep(force_value_delta)
+while True:
+    speed_val = client.read_holding_registers(register.energy_speed).registers[0]
+    if speed_val != 100:
+        client.write_registers(register.energy_speed, 100)
+    time.sleep(0.5)
